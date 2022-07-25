43d1b58854887c66015f2b0dc30564710a00d982
YAKUP KOSE <yakupkose3123@gmail.com>
2022-07-25 13:42:24 +0300

    first commit

---
+++ main/settings/prod.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2", 
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),   
+        "PASSWORD": config("SQL_PASSWORD"),  
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
+        "ATOMIC_REQUESTS": True,
+    }
+}
+
+#! For debug-toolbar
+INTERNAL_IPS = [
+ "127.0.0.1",
+]
+
+
